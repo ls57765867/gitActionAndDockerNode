d3b72a54f3be1a81167bdeb590cf6ebac7e34d69
liushuai <ls57765867@126.com>
2022-02-27 01:12:57 +0800

    adduser work

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const router = new Router(); // 创建路由，支持传递参数
 const mysql = require('mysql');
 const {body} = require("koa/lib/response");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'crouch11',
-    database: 'test'
-});
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'crouch11',
+//     database: 'test'
+// });
+//
+// connection.connect();
+//
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+// });
 
 // 指定一个url匹配
 router.get('/test', async (ctx) => {
@@ -36,25 +36,25 @@ router.get('/aaa', async (ctx) => {
     }
 })
 // 指定一个url匹配
-router.get('/test2', async (ctx) => {
-    //查，数据库代码
-    var sql = 'SELECT name FROM test';
-    const data = await new Promise(resolve => {
-        connection.query(sql, function (err, res) { //询问访问数据库，也就是去嫩那个数据库
-            if (err) { //失败就报个错。
-                console.log('[SELECT ERROR] - ', err.message);
-                return;
-            }
-            console.log("数据库查的结果：");
-            console.log(res); //成功就输出下结果
-            resolve(res)
-        });
-    })
-    ctx.body = {
-        code: 0,
-        msg: data
-    }
-})
+// router.get('/test2', async (ctx) => {
+//     //查，数据库代码
+//     var sql = 'SELECT name FROM test';
+//     const data = await new Promise(resolve => {
+//         connection.query(sql, function (err, res) { //询问访问数据库，也就是去嫩那个数据库
+//             if (err) { //失败就报个错。
+//                 console.log('[SELECT ERROR] - ', err.message);
+//                 return;
+//             }
+//             console.log("数据库查的结果：");
+//             console.log(res); //成功就输出下结果
+//             resolve(res)
+//         });
+//     })
+//     ctx.body = {
+//         code: 0,
+//         msg: data
+//     }
+// })
 
 
 // 调用router.routes()来组装匹配好的路由，返回一个合并好的中间件
@@ -68,6 +68,6 @@ app.use(router.allowedMethods({
 
 // 启动服务监听本地3000端口
 app.listen(3000, () => {
-    console.log('sbsb4')
+    console.log('sbsb5')
     console.log('应用已经启动，http://localhost:3033');
 })
